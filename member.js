3e75eaa7edd51836133519c53465bb80913b7957
Sajja Aditya <adityasajja6@gmail.com>
2024-10-25 12:00:50 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var member = {
+        name: 'John Doe',
+        age: 30,
+        skills: ['HTML', 'CSS', 'JS'],
+        salary: 35000
+    };
+    return member;
+}
